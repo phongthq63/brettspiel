ff8bbcda48659e0b4e00bf75dcbc41504b1aa6a9
Quách Thanh Phong <thanhphong216@gmail.com>
2025-04-25 18:34:15 +0700

    no message

---
+++ boardgame-guide/next-sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    siteUrl: "https://brettspiel.thanhphong.com",
+    generateRobotsTxt: true,
+    sitemapSize: 5000,
+};
--- boardgame-guide/src/app/layout.tsx
+++ boardgame-guide/src/app/layout.tsx
@@ -16,8 +16,35 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: "Your App",
-  description: "Awesome gamedetail experience",
+  title: "Web Boardgame - Discover and Play",
+  description: "Explore a wide range of board games, find detailed information, and connect with other players. Your ultimate board game companion!",
+  keywords: [
+    "board games",
+    "play board games",
+    "board game community",
+    "game details",
+    "multiplayer games"
+  ],
+  openGraph: {
+    title: "Web Boardgame - Discover and Play",
+    description: "Explore a wide range of board games, find detailed information, and connect with other players. Your ultimate board game companion!",
+    url: "https://yourwebsite.com",
+    type: "website",
+    images: [
+      {
+        url: "https://yourwebsite.com/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Web Boardgame Banner",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Web Boardgame - Discover and Play",
+    description: "Explore a wide range of board games, find detailed information, and connect with other players. Your ultimate board game companion!",
+    images: ["https://yourwebsite.com/og-image.jpg"],
+  },
 };
 
 export default function RootLayout({
